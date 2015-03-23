a445327599460a80294de838a0f90866b992a5eb
Virindh Borra <borra.virindh@gmail.com>
2015-03-23 17:04:25 -0400

    Initial Commit

--- Message Spammer/AppDelegate.h
+++ Message Spammer/AppDelegate.h
@@ -10,6 +10,40 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
+//The property for the message text field
+@property (weak) IBOutlet NSTextField *messageTextBox;
+
+//The property for the recipient text box
+@property (weak) IBOutlet NSTextField *buddyTextBox;
+
+//Property for the number of texts to send
+@property (weak) IBOutlet NSTextField *numOfTextsTextBox;
+
+//Property for the delay text box
+@property (weak) IBOutlet NSTextField *delaytextBox;
+
+//Called when the start button is pressed, initiates the process
+- (IBAction)startButton:(id)sender;
+
+// The label that shows the # of texts sent
+@property (weak) IBOutlet NSTextField *textsLabel;
+
+//A progress bar to show...progress
+@property (weak) IBOutlet NSProgressIndicator *progressBar;
+
+//To keep track of the # of texts alreday sent
+@property int numOfTextsSent;
+
+/*
+ The Local machines service ID from the messages app. This is machine specific, and service specific(eg. Text v. iMessage). You can get these ID's by running this Apple Script:
+ 
+ Apple Script:
+ tell application "Messages"
+ get id of every service
+ end tell
+ 
+*/
+@property (weak) NSString *serviceID;
 
 @end
 
--- Message Spammer/AppDelegate.m
+++ Message Spammer/AppDelegate.m
@@ -15,12 +15,70 @@
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+//Synthesize variables, obviously not needed with auto-synthesize, but i'm old school like that 😎. Complain to me @virindh.
+@synthesize progressBar;
+@synthesize textsLabel;
+@synthesize delaytextBox;
+@synthesize numOfTextsTextBox;
+@synthesize numOfTextsSent;
+@synthesize messageTextBox;
+@synthesize buddyTextBox;
+@synthesize serviceID;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    //Set the service ID to the service you want to spam.
+   serviceID = @"4D1FA49E-1095-4D0E-BEBA-FD9FB2D02A64";//This is just a sample, replace with the ID you want.
+
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
-
+- (IBAction)startButton:(id)sender
+{
+    //Start the progress bar at 0
+    [progressBar setDoubleValue:0];
+    
+    //Get the number of texts to send
+    int numOfTextsToSend = [numOfTextsTextBox intValue];
+    
+    //Get the delay between each text
+    int delayInSeconds = [delaytextBox intValue];
+    
+    //Set the progress bar's max value to be the number of texts to send to get the progression correct
+    [progressBar setMaxValue:numOfTextsToSend];
+    for (int i = 1; i <= numOfTextsToSend; i++)
+    {
+        //Send one text
+        [self sendText];
+        
+        //Update the progress bar
+        [progressBar setDoubleValue:i];
+        
+        //Update the UI(the label)
+        [textsLabel setStringValue:[NSString stringWithFormat:@"%i Texts", i]];
+        
+        //Delay between the next tezt, default is 0
+        [NSThread sleepForTimeInterval:delayInSeconds];
+        
+    }
+}
+//Sends one text
+-(void)sendText
+{
+    //The message to send
+    NSString *messageToSend = messageTextBox.stringValue;
+    
+    //The person to send the message to
+    NSString *buddyToSend = buddyTextBox.stringValue;
+    
+    //Creates the source code for the apple script
+    NSString *sourceCodeForAppleScript = [NSString stringWithFormat:@"tell application \"Messages\" \n" @"set theBuddy to buddy \"%@\" of service id \"%@\"\n" @"send \"%@\" to theBuddy \n" @"end tell \n",buddyToSend,serviceID, messageToSend];
+    //Create the NSAppleScript Object
+    NSAppleScript *appleScript = [[NSAppleScript alloc] initWithSource:sourceCodeForAppleScript];
+    
+    //Run the Apple Script
+    [appleScript executeAndReturnError:nil];
+}
 @end
